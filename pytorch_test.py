66d989ade077d980e58179b9a1e3bc4a93a2d673
Lucas Menger <57370662+Potatotime13@users.noreply.github.com>
2024-08-20 15:45:51 +0200

    quantum conv

--- pytorch_test.py
+++ pytorch_test.py
@@ -1,81 +1,17 @@
 # %%
 
 from qiskit import QuantumCircuit
-from qiskit.quantum_info import SparsePauliOp
 from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
-
-# Create a new circuit with two qubits
-qc = QuantumCircuit(4)
-
-# Add a Hadamard gate to qubit 0
-qc.h(0)
-qc.h(1)
-qc.h(2)
-qc.h(3)
-
-# Add rotation gates to qubit 1
-qc.rz(0.5, 0)
-qc.rz(0.5, 1)
-qc.rz(0.5, 2)
-qc.rz(0.5, 3)
-
-# add rzz rotations to qubit 1
-qc.rzz(0.5, 0, 1)
-qc.rzz(0.5, 0, 2)
-qc.rzz(0.5, 0, 3)
-qc.rzz(0.5, 1, 2)
-qc.rzz(0.5, 1, 3)
-qc.rzz(0.5, 2, 3)
-
-# Add rotation gates to qubit 1
-qc.rx(0.5, 0)
-qc.rx(0.5, 1)
-qc.rx(0.5, 2)
-qc.rx(0.5, 3)
-
-# Perform a controlled-X gate on qubit 1, controlled by qubit 0
-qc.cx(0, 1)
-qc.cx(1, 2)
-qc.cx(2, 3)
-qc.cx(3, 0)
-
-# add measurement gates
-qc.measure_all()
-
-# Return a drawing of the circuit using MatPlotLib ("mpl"). This is the
-# last line of the cell, so the drawing appears in the cell output.
-# Remove the "mpl" argument to get a text drawing.
-qc.draw("mpl")
-
-
-from qiskit import QuantumCircuit, transpile
+from qiskit.circuit import Parameter
 from qiskit_aer import AerSimulator
-from qiskit.visualization import plot_histogram, plot_state_city
-import qiskit.quantum_info as qi
-
-simulator = AerSimulator()
-circ = transpile(qc, simulator)
-
-# Run and get counts
-result = simulator.run(circ).result()
-counts = result.get_counts(circ)
-plot_histogram(counts, title='Bell-State counts')
-
-# %%
 import numpy as np
 import torch
 import torch.nn as nn
+from torchvision import datasets, transforms
+from torch import nn, optim
+from tqdm import tqdm
 
-"""
-conv2d network idea
 
-conv2d 28x28x1, stride 2 -> 14x14x (num_qchannels + num_cchannels)
-conv2d 14x14x(num_qchannels + num_cchannels), stride 2 -> 7x7x (num_qchannels + num_cchannels)
-conv2d 7x7x(num_qchannels + num_cchannels), stride 2 -> 3x3x (num_qchannels + num_cchannels)
-flatten 3x3x(num_qchannels + num_cchannels) -> 9(num_qchannels + num_cchannels)
-fc 9(num_qchannels + num_cchannels) -> output_size
-
-"""
 class QuantumConv2d(nn.Module):
     def __init__(self, kernel_size, stride):
         super(QuantumConv2d, self).__init__()
@@ -83,90 +19,173 @@ class QuantumConv2d(nn.Module):
         self.stride = stride
         self.weight = nn.Parameter(torch.randn(kernel_size, kernel_size))
         self.simulator = AerSimulator()
+        self.circ = self.build_circuit()
 
-    def build_circuit(self, x: torch.Tensor):
-        x = x.flatten().numpy()
-        qc = QuantumCircuit(len(x))
-        for i in range(len(x)):
+    def build_circuit(self):
+        lens = self.kernel_size**2
+        qc = QuantumCircuit(lens)
+        for i in range(lens):
             qc.h(i)
 
-        for i, val in enumerate(x):
-            qc.rz(val,i)
+        for i in range(lens):
+            angle = Parameter('enc_0_'+str(i))
+            qc.rz(angle,i)
         
-        for i in range(len(x)-1):
-            for j in range(i+1, len(x)):
-                qc.rzz(x[i]*x[j], i, j)
+        for i in range(lens-1):
+            for j in range(i+1, lens):
+                angle = Parameter('enc_1_'+str(i)+'_'+str(j))
+                qc.rzz(angle, i, j)
 
-        for i in range(len(self.weight)):
-            qc.rx(self.weight.flatten()[i].item(), i)
+        for i in range(lens):
+            angle = Parameter('conv_'+str(i))
+            qc.rx(angle, i)
 
-        for i in range(len(x)):
-            qc.cx(i, (i+1)%len(x))
+        for i in range(lens):
+            qc.cx(i, (i+1) % lens)
 
         qc.measure_all()
+        qc = generate_preset_pass_manager(optimization_level=3).run(qc)
+
         return qc
     
     def bin_to_num(self, list_x: list):
         return [int(x, 2) for x in list_x]
     
+    def str_to_tensor(self, str_x: str):
+        res = []
+        for x in str_x:
+            res.append([int(y) for y in x])
+        return torch.tensor(res, dtype=torch.float32)
+    
+    def assign_params(self, x):
+        params = {}
+        lens = self.kernel_size**2
+        x_flat = x.flatten()
+        w_flat = self.weight.flatten()
+        for i in range(lens):
+            params['enc_0_'+str(i)] = x_flat[i].item()
+
+        for i in range(lens-1):
+            for j in range(i+1, lens):
+                params['enc_1_'+str(i)+'_'+str(j)] = x_flat[i].item() * x_flat[j].item()
+
+        for i in range(lens):
+            params['conv_'+str(i)] = w_flat[i].item()
+
+        return self.circ.assign_parameters(params)
+    
     def sub_forward(self, x):
-        x = self.build_circuit(x)
-        circ = transpile(qc, simulator)
+        circ = self.assign_params(x)
         # Run and get counts
-        result = simulator.run(circ).result()
+        result = self.simulator.run(circ).result()
         counts = result.get_counts(circ)
 
-        count_bins = torch.tensor(self.bin_to_num(list(counts.keys())), dtype=torch.float32)
+        count_bins = self.str_to_tensor(list(counts.keys()))
         count_vals = torch.tensor(list(counts.values()), dtype=torch.float32)
         count_vals = count_vals / count_vals.sum()
-        return torch.matmul(count_bins, count_vals)
+        return torch.matmul(count_vals, count_bins)
     
     def forward(self, x):
-        out = torch.zeros(x.shape[0], x.shape[1]//self.stride, x.shape[2]//self.stride)
+        out = torch.zeros(x.shape[0], self.kernel_size**2, x.shape[1]//self.stride, x.shape[2]//self.stride)
         for b in range(x.shape[0]):
             for i in range(0, x.shape[1], self.stride):
                 for j in range(0, x.shape[2], self.stride):
-                    out[b, i//self.stride, j//self.stride] = self.sub_forward(x[b, i:i+self.kernel_size, j:j+self.kernel_size])
+                    out[b, :, i//self.stride, j//self.stride] = self.sub_forward(x[b, i:i+self.kernel_size, j:j+self.kernel_size])
         return out
 
-# Create a simple neural network
-class SimpleNN(nn.Module):
+
+class QuantumConvNet(nn.Module):
     def __init__(self):
-        super(SimpleNN, self).__init__()
-        self.custom = QuantumConv2d(2, 2)
-        self.fc2 = nn.Linear(25, 1)
+        super(QuantumConvNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 1, 2, 2, 2)
+        self.conv2 = nn.Conv2d(1, 1, 2, 2)
+        self.qconv2 = QuantumConv2d(2, 2)
+        self.fc1 = nn.Linear(64, 10)
     
-    def forward(self, x):
-        x = torch.relu(x)
-        x = self.custom(x)
+    def forward(self, x):  
+        x = torch.relu(self.conv1(x))
+        x = torch.relu(self.conv2(x))
+        x = x.squeeze(1)
+        x = torch.relu(self.qconv2(x))
+        # 4 * 4 * 4 = 64
         x = x.flatten(1)
-        x = self.fc2(x)
+        x = torch.softmax(self.fc1(x), dim=-1)
         return x
-    
-# Create a simple dataset
-X = torch.randn(100, 10, 10)
-y = torch.randn(100, 1)
 
-# dataset
-dataset = torch.utils.data.TensorDataset(X, y)
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=10, shuffle=True)
 
-# train the model
-model = SimpleNN()
-criterion = nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+class ConvNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 1, 2, 2)
+        self.conv2 = nn.Conv2d(1, 1, 2, 2)
+        self.fc1 = nn.Linear(49, 10)
 
-for epoch in range(10):
-    for i, (X_batch, y_batch) in enumerate(dataloader):
-        y_pred = model(X_batch)
-        loss = criterion(y_pred, y_batch)
+    def forward(self, x):
+        x = torch.relu(self.conv1(x))
+        x = torch.relu(self.conv2(x))
+        x = x.flatten(1)
+        x = torch.softmax(self.fc1(x), dim=-1)
+        return x
+
+#%%
+
+
+transform = transforms.Compose([transforms.ToTensor(),
+                              transforms.Normalize((0.5,), (0.5,)),
+                              ])
+
+trainset = datasets.MNIST('./', download=True, train=True, transform=transform)
+# cut dataset to 1000 samples
+trainset.data = trainset.data[:100]
+valset = datasets.MNIST('./', download=True, train=False, transform=transform)
+# cut dataset to 100 samples
+valset.data = valset.data[:10]
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=10, shuffle=True)
+valloader = torch.utils.data.DataLoader(valset, batch_size=10, shuffle=True)
+
+net = ConvNet()
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(net.parameters(), lr=0.001)
+
+for epoch in tqdm(range(10)):
+    running_loss = []
+    for i, (X_batch, y_batch) in enumerate(trainloader):
         
         optimizer.zero_grad()
+
+        outputs = net(X_batch)
+        loss = criterion(outputs, y_batch)
         loss.backward()
         optimizer.step()
+
+        running_loss.append(loss.item())
+
+    print(f'Epoch: {epoch}, Loss: {np.mean(running_loss)}')
+    running_loss = []
+
+print('Finished Training')
+
+#%%
+
+qnet = QuantumConvNet()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(qnet.parameters(), lr=0.001)
+
+for epoch in tqdm(range(10)):
+    running_loss = []
+    for i, (X_batch, y_batch) in enumerate(trainloader):
         
-        if i % 10 == 0:
-            print(f'Epoch: {epoch}, Loss: {loss.item()}')
+        optimizer.zero_grad()
 
+        outputs = qnet(X_batch)
+        loss = criterion(outputs, y_batch)
+        loss.backward()
+        optimizer.step()
 
+        running_loss.append(loss.item())
 
+    print(f'Epoch: {epoch}, Loss: {np.mean(running_loss)}')
+    running_loss = []
+
+print('Finished Training')
